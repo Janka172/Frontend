8249b1606924c4db346f44f91009c054264bc3fc
Janka172 <jankahegedus872@gmail.com>
2024-12-27 15:51:37 +0100

    Update Szuro.js

--- src/alkalmazasok/Szuro.js
+++ src/alkalmazasok/Szuro.js
@@ -2,11 +2,32 @@ import React, { useState, useEffect } from 'react';
 import SzuroStilus from './Szuro.css';
 
 function Szuro() {
-  //Adatbázisból kinyert: nevek
-  var mindenKategoria=['-','Kategoria 1', 'Kategoria 2', 'Kategoria 3', 'Kategoria 4', 'Kategoria 5', 'Kategoria 5', 'Kategoria 6', 'Kategoria 7', 'Kategoria 8'];
-  var mindenVideokartya=['-','Videokartya 1', 'Videokartya 2', 'Videokartya 3', 'Videokartya 4', 'Videokartya 5', 'Videokartya 5', 'Videokartya 6', 'Videokartya 7', 'Videokartya 8'];
-  var mindenProcesszor=['-','Processzor 1', 'Processzor 2', 'Processzor 3', 'Processzor 4', 'Processzor 5', 'Processzor 5', 'Processzor 6', 'Processzor 7', 'Processzor 8'];
-  var mindenOpRendszer=['-','OpRendszer 1', 'OpRendszer 2', 'OpRendszer 3', 'OpRendszer 4', 'OpRendszer 5', 'OpRendszer 5', 'OpRendszer 6', 'OpRendszer 7', 'OpRendszer 8'];
+  // Adatbázisból kinyert: nevek
+  const [mindenKategoria, setMindenKategoria] = useState([]);
+  const [loadingK, setLoadingK] = useState(true);
+
+  var mindenVideokartya = ['-', 'Videokartya 1', 'Videokartya 2', 'Videokartya 3', 'Videokartya 4', 'Videokartya 5', 'Videokartya 5', 'Videokartya 6', 'Videokartya 7', 'Videokartya 8'];
+  var mindenProcesszor = ['-', 'Processzor 1', 'Processzor 2', 'Processzor 3', 'Processzor 4', 'Processzor 5', 'Processzor 5', 'Processzor 6', 'Processzor 7', 'Processzor 8'];
+  var mindenOpRendszer = ['-', 'OpRendszer 1', 'OpRendszer 2', 'OpRendszer 3', 'OpRendszer 4', 'OpRendszer 5', 'OpRendszer 5', 'OpRendszer 6', 'OpRendszer 7', 'OpRendszer 8'];
+
+  // A kategoria adatok lekérése
+  async function getMindenKategoria() {
+    try {
+      const response = await fetch("https://localhost:44316/api/Kategoria");
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      setMindenKategoria(data);  // Frissítjük a state-et
+      setLoadingK(false);  // Beállítjuk, hogy az adatok betöltődtek
+    } catch (error) {
+      console.error('Unable to get items.', error);
+    }
+  }
+  useEffect(() => {
+    getMindenKategoria();  // Az API hívás az adatok betöltéséhez
+  }, []);  // Csak egyszer fut le, amikor a komponens betöltődik
 
   const [nev, setNev] = useState('');
   const [kategoria, setKategoria] = useState('-');
@@ -21,20 +42,19 @@ function Szuro() {
   const [hatter, setHatter] = useState('rgb(194, 52, 52, 0.5)');
   const [displ, setDispl] = useState('block');
   const [megnyitva, setMegnyitva] = useState(true);
-  
+
   useEffect(() => {
     if (kicsie) {
       setMegnyitva(false);
       setDispl('none');
       setHatter('rgb(194, 52, 52, 0)');
-    }
-    else{
+    } else {
       setMegnyitva(true);
       setDispl('block');
       setHatter('rgb(194, 52, 52, 0.5)');
     }
   }, [kicsie]);
-  
+
   function menuMegnyitas() {
     if (!megnyitva) {
       setMegnyitva(true);
@@ -56,7 +76,7 @@ function Szuro() {
     setRam('');
     setTarhely('');
   }
-  
+
   return (
     <div className='filter' style={{ backgroundColor: hatter }}>
       <div>
@@ -66,7 +86,7 @@ function Szuro() {
         <div className='kisMeret'>
           <button className='bezaros' onClick={menuMegnyitas}>Szűrő beállítások</button>
         </div>
-      </div>  
+      </div>
 
       <div className='mezo' style={{ display: displ }}>
         <p>Név:</p>
@@ -76,7 +96,11 @@ function Szuro() {
       <div className='mezo' style={{ display: displ }}>
         <p>Kategória:</p>
         <select id='kategoriaCombo' value={kategoria} onChange={(e) => setKategoria(e.target.value)}>
-          {mindenKategoria.map((katNev, index) => (<option value={katNev} key={index}>{katNev}</option>))}
+          {loadingK ? (<option>Betöltés...</option>) : (
+            mindenKategoria.map((kat, index) => (
+              <option value={kat.Nev} key={index}>{kat.Nev}</option> 
+            ))
+          )}
         </select>
       </div>
 
@@ -113,7 +137,6 @@ function Szuro() {
 
       <button className='gombocska' style={{ display: displ }}>Keresés</button>
       <button className='gombocska' style={{ display: displ }} onClick={mezokTorlese}>Mégsem</button>
-          
     </div>
   );
 }
